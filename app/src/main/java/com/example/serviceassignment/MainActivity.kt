89dbae2431f3e87ed9604df8b9f3e1cadc4ecc0c
Akshay-Rajeev-Nambiar <akshay.rajeev@ymedialabs.com>
2020-10-17 18:29:39 +0530

    updated commit

--- app/src/main/java/com/example/serviceassignment/MainActivity.kt
+++ app/src/main/java/com/example/serviceassignment/MainActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mediaPlayer: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
